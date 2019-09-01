890a71bb7fd018a1eb147a993e16a73bd0f1f1e8
Federico Molina <federicomolinachavez@gmail.com>
2019-09-01 15:55:31 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/Recurso.png" class="imge">
+    <Title />
+    <Section :sectionContent = "sectionContent"/>
+    <Links :vals= "linkss" />
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Title from './components/Title'
+import 'semantic-ui-css/semantic.min.css';
+import Section from './components/Section'
+import Footer from './components/footer'
+import Links from './components/links'
 export default {
   name: 'app',
+  data: ()=>{
+    return{
+      linkss : [
+      {link: "https://drive.google.com/open?id=10b-7gfpazXnhQf_5NAH8kevu1hk32jj8", desc: "item 1"},
+      {link: "https://drive.google.com/open?id=10b1qtgHCtgf1BLLsn9BFjUsIaM_YH0kS" , desc: "item 2"},
+      {link: "https://drive.google.com/open?id=10JPRSkUJYCHJ1X9_FNMeoTVDQKuU0ipv", desc: "item3"}
+    ],
+     sectionContent : [
+       {imgDir : "1.jpeg"},
+       {imgDir : "2.jpeg"},
+       {imgDir : "3.jpeg"},
+       {imgDir : "4.jpeg"},
+       {imgDir : "5.jpeg"},
+       {imgDir : "6.jpeg"},
+       {imgDir : "7.jpeg"},
+       {imgDir : "8.jpeg"},
+       {imgDir : "9.jpeg"},
+       {imgDir : "10.jpeg"}
+     ]
+    }
+  },
   components: {
-    HelloWorld
+    Title,
+    Section,
+    Footer,
+    Links 
   }
 }
 </script>
@@ -24,5 +54,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.imge {
+  width: 300px;
+  height: 300px;
 }
 </style>
---
+++ src/components/footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="footerStyle"> 
+        <p class="letter">Todos los derechos del material son reservados para Keep Safe SAS</p>
+        <a href="https://keepsafeco.org">Página web Keep Safe</a>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+<style scoped>
+    .footerStyle{
+        width: 100%;
+        background-color: black;
+        margin-top: 10%;
+    }
+    .letter{
+        color: white;
+    }
+</style>
